d7e3d3d6b825ef7b208751d9f3ba88c9171a26aa
simkaa47 <simkaa7@gmail.com>
2023-03-27 17:38:37 +0300

    Created IRepository interface

---
+++ src/ArtExchange.Application/Contracts/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace ArtExchange.Application.Contracts.Repository
+{
+    internal interface IRepositoryAsync<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<T> GetFirstWhere(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> GetWhere(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ src/ArtExchange.Application/Feautures/Persons/Commands/CreatePersonCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace ArtExchange.Application.Feautures.Persons.Commands
+{
+    public class CreatePersonCommand:IRequest<int>
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string ImageUri { get; set; } = string.Empty;
+        public DateTime DataOfBirth { get; set; }
+    }
+}
