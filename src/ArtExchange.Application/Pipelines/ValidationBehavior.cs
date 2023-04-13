a47bfa30dda372ea6f903ec36eef4a0af661740a
Serafim Prilezhaev <simkaa7@gmail.com>
2023-04-13 21:31:09 +0300

    Validation pipeline was added

---
+++ src/ArtExchange.Application/Pipelines/ValidationBehavior.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace ArtExchange.Application.Pipelines
+{
+    public sealed class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : class
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators) => _validators = validators;
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (!_validators.Any())
+            {
+                return await next();
+            }
+            var context = new ValidationContext<TRequest>(request);
+            var errors = _validators
+                .Select(x => x.Validate(context))
+                .SelectMany(x => x.Errors);
+                
+            if (errors.Any())
+            {
+                throw new ValidationException("Server validation error", errors);
+            }
+            return await next();
+
+            //https://code-maze.com/cqrs-mediatr-fluentvalidation/
+        }
+
+        
+    }
+    
+}
