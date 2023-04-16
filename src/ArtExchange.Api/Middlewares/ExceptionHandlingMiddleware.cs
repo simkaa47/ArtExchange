5c5bbb75b61d7d77352d125251284e0f90257fa4
Serafim Prilezhaev <simkaa7@gmail.com>
2023-04-16 22:31:10 +0300

    Exception Middlware was added and registerd in pipeline

---
+++ src/ArtExchange.Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,81 @@
+﻿using FluentValidation;
+using System.Linq;
+using System.Text.Json;
+
+namespace ArtExchange.Api.Middlewares
+{
+    public class ExceptionHandlingMiddleware : IMiddleware
+    {
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                await HandleExceptionAsync(context, e);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext httpContext, Exception exception)
+        {
+            var statusCode = GetStatusCode(exception);
+            var response = new
+            {
+                title = GetTitle(exception),
+                status = statusCode,
+                detail = exception.Message,
+                errors = GetErrors(exception)
+            };
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = statusCode;
+            await httpContext.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+
+        public static int GetStatusCode(Exception exception) 
+        {
+            switch (exception)
+            {
+                case BadHttpRequestException:return StatusCodes.Status400BadRequest;  
+                    case ValidationException:return StatusCodes.Status422UnprocessableEntity;                    
+                default:return StatusCodes.Status500InternalServerError;                    
+            }
+        }
+
+        private static string GetTitle(Exception exception)
+        {
+            switch (exception)
+            {
+                case ApplicationException applicationException:return applicationException.Message;
+                default:return "Server error";
+            }
+        }
+
+        private static IReadOnlyDictionary<string, string[]> GetErrors(Exception exception)
+        {
+            IReadOnlyDictionary<string, string[]>? errors = null;
+            if (exception is ValidationException validationException)
+            {
+                errors = validationException.Errors
+                    .GroupBy(
+                            x => x.PropertyName,
+                            x => x.ErrorMessage,
+                            (propertyName, errorMessages) => new
+                            {
+                                Key = propertyName,
+                                Values = errorMessages.Distinct().ToArray()
+                            })
+                    .ToDictionary(x => x.Key, x => x.Values);
+            }
+            return errors;
+        }
+    }
+}
