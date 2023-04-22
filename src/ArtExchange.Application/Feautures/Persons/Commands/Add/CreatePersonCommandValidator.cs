c34390526455eb9a66791a36b9f049f85888209f
Serafim Prilezhaev <simkaa7@gmail.com>
2023-04-22 19:15:39 +0300

    Checking for unique email was added

--- src/ArtExchange.Application/Feautures/Persons/Commands/Add/CreatePersonCommandValidator.cs
+++ src/ArtExchange.Application/Feautures/Persons/Commands/Add/CreatePersonCommandValidator.cs
@@ -1,32 +1,49 @@
-﻿using FluentValidation;
+﻿using ArtExchange.Application.Contracts.Repository;
+using ArtExchange.Domain.Entities;
+using FluentValidation;
 
 namespace ArtExchange.Application.Feautures.Persons.Commands.Add
 {
     public class CreatePersonCommandValidator:AbstractValidator<CreatePersonCommand>
     {
-        public CreatePersonCommandValidator()
+        private readonly IRepositoryAsync<Person> _personRepository;
+
+        public CreatePersonCommandValidator(IRepositoryAsync<Person> personRepository)
         {
             RuleFor(command =>
                 command.FirstName).NotEmpty()
                 .MaximumLength(250)
-                .WithErrorCode("Player first name must not be empty");
+                .WithMessage("Player first name must not be empty");
 
             RuleFor(command =>
                 command.LastName).NotEmpty()
                 .MaximumLength(250)
-                .WithErrorCode("Player last name must not be empty");
+                .WithMessage("Player last name must not be empty");
 
             RuleFor(command =>
                 command.DataOfBirth)
                 .LessThanOrEqualTo(DateTime.Now)
-                .WithErrorCode("Date of birth is incorrect");
+                .WithMessage("Date of birth is incorrect");
 
             RuleFor(command =>
                 command.Email).NotEmpty()
                 .MaximumLength(250)
                 .EmailAddress()
-                .WithErrorCode("Incorrect email");
+                .WithMessage("Incorrect email");
 
+            RuleFor(command =>
+                command.Email).Must(IsEmailUnique)
+                .WithMessage(command=>$"Email {command.Email} already belongs to other person");
+
+
+
+            _personRepository = personRepository;
+        }
+
+        public bool IsEmailUnique(string email)
+        {
+            var result =  _personRepository.GetFirstWhere(p => p.Email == email);
+            return result.Result == null;
         }
     }
 }
